5fd7876f8cc6c9b06968d22e4c76ea9fe13c1a11
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-08-17 10:14:44 +0430

    first version added

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,39 +1,43 @@
-/* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
+import Button from './Button';// Adjust the path based on your project structure
 import '../style/ButtonStyles.css';
+import calculate from '../logic/calculate'; // Adjust the path to calculate.js
 
 function Calculator() {
+  const [calculatorState, setCalculatorState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (buttonName) => {
+    const newState = calculate(calculatorState, buttonName);
+    setCalculatorState(newState);
+  };
+
   return (
     <div className="container">
-      <div className="header">0</div>
-      <Button className="button" label="AC" />
-      <Button className="button" label="+/-" />
-      <Button className="button" label="%" />
-      <Button className="button" label="/" />
-      <Button className="button" label="7" />
-      <Button className="button" label="8" />
-      <Button className="button" label="9" />
-      <Button className="button operator" label="x" />
-      <Button className="button" label="4" />
-      <Button className="button" label="5" />
-      <Button className="button" label="6" />
-      <Button className="button operator" label="-" />
-      <Button className="button" label="1" />
-      <Button className="button" label="2" />
-      <Button className="button" label="3" />
-      <Button className="button operator" label="+" />
-      <Button className="button zero" label="0" />
-      <Button className="button" label="." />
-      <Button className="button operator" label="=" />
+      <div className="header">{calculatorState.next || calculatorState.total || '0'}</div>
+      <Button className="button" label="AC" onClick={handleClick} />
+      <Button className="button" label="+/-" onClick={handleClick} />
+      <Button className="button" label="%" onClick={handleClick} />
+      <Button className="button" label="/" onClick={handleClick} />
+      <Button className="button" label="7" onClick={handleClick} />
+      <Button className="button" label="8" onClick={handleClick} />
+      <Button className="button" label="9" onClick={handleClick} />
+      <Button className="button operator" label="x" onClick={handleClick} />
+      <Button className="button" label="4" onClick={handleClick} />
+      <Button className="button" label="5" onClick={handleClick} />
+      <Button className="button" label="6" onClick={handleClick} />
+      <Button className="button operator" label="-" onClick={handleClick} />
+      <Button className="button" label="1" onClick={handleClick} />
+      <Button className="button" label="2" onClick={handleClick} />
+      <Button className="button" label="3" onClick={handleClick} />
+      <Button className="button operator" label="+" onClick={handleClick} />
+      <Button className="button zero" label="0" onClick={handleClick} />
+      <Button className="button" label="." onClick={handleClick} />
+      <Button className="button operator" label="=" onClick={handleClick} />
     </div>
-  );
-}
-
-function Button({ className, label }) {
-  return (
-    <button className={className} type="button">
-      {label}
-    </button>
   );
 }
 
