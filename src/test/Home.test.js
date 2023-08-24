3275c4ef89c800fbcf6787547bf4aeb02ead3f74
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-08-24 17:30:16 +0430

    homeTest updated

--- src/test/Home.test.js
+++ src/test/Home.test.js
@@ -6,3 +6,21 @@ test('render correctly', () => {
   const homeComponent = render(<Home />);
   expect(homeComponent).toMatchSnapshot();
 });
+
+test('renders welcome message', () => {
+  const { getByText } = render(<Home />);
+  const welcomeMessage = getByText('Welcome to MathMagicians');
+  expect(welcomeMessage).toBeInTheDocument();
+});
+
+test('renders paragraphs', () => {
+  const { getAllByText } = render(<Home />);
+  const paragraphs = getAllByText((content, element) => element.tagName.toLowerCase() === 'p');
+  expect(paragraphs).toHaveLength(5);
+});
+
+test('renders specific content', () => {
+  const { getByText } = render(<Home />);
+  const specificContent = getByText(/Step into the captivating realm of Math Magician/);
+  expect(specificContent).toBeInTheDocument();
+});
