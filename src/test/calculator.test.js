a22887aee6d8f8b91c1da2114279ccff808497e0
Jlberbesi <jlberbesi.walloom@gmail.com>
2023-08-24 15:24:48 -0500

    Add calculate function second round

--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -8,3 +8,13 @@ describe('calculate function', () => {
     const result = calculate({ total: '10', next: '5', operation: '+' }, 'AC');
     expect(result).toEqual({ total: null, next: null, operation: null });
   });
+
+  it('should handle number buttons', () => {
+    const result = calculate({ total: '10', next: '5', operation: '+' }, '2');
+    expect(result).toEqual({ total: '10', next: '52', operation: '+' });
+  });
+
+  it('should handle decimal button', () => {
+    const result = calculate({ total: '10', next: '5', operation: '+' }, '.');
+    expect(result).toEqual({ total: '10', next: '5.', operation: '+' });
+  });
